1856cf4c1dce736110c5c43c7218f5f972da699e
Facundo Malgieri <facundo.malgieri@globant.com>
2017-02-24 18:05:05 -0300

    last changes

--- app/certificates/certificates-details/certificates-details.component.ts
+++ app/certificates/certificates-details/certificates-details.component.ts
@@ -1,31 +1,38 @@
 import { CertificatesService } from './../certificates-services/certificates.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription'
 
 @Component({
     moduleId: module.id,
     templateUrl: 'certificates-details.component.html',
-    styleUrls: [ 'certificates-details.component.css' ]
-}) 
+    styleUrls: ['certificates-details.component.css']
+})
 
 export class CertificatesDetailsComponent {
     certificate: any;
     private sub: Subscription;
 
-    constructor(private _route: ActivatedRoute, 
-                private _router: Router,
-                private _certificatesService: CertificatesService) { 
+    constructor(private _route: ActivatedRoute,
+        private _router: Router,
+        private _certificatesService: CertificatesService) {
     }
-    
+
     ngOnInit(): void {
-        this.certificate = this._certificatesService.getCertificate(+this._route.snapshot.params['id']);
+        this._route.params.forEach((params: Params) => {
+            this.certificate = this._certificatesService.getCertificate(+params['id']);
+        })
     }
-    
+
     onBack(): void {
         this._router.navigate(['/certificates']);
     }
+
+    next(): void {
+        let obj = this._certificatesService.getCertificate(+this._route.snapshot.params['id']);
+        let id = obj.id + 1;
+        this._router.navigate(['/certificates/' + id]);
+    }
 }
-   
-    
-  
+
+
